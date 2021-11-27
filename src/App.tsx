b83612851b09090cc4d7d629a515d822306ef55f
Deepak Babhale <deepak.babhale123@gmail.com>
2021-11-27 15:11:30 +0100

    code optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
-import ScrollComponent from './ScrollComponent';
+import React from "react";
+import "./App.css";
+import ScrollComponent from "./ScrollComponent";
 
 function App() {
   return (
     <div className="app">
-      <ScrollComponent apiUrl={'https://jsonplaceholder.typicode.com/photos'} pageNumber={'_page'} pageLimit={'_limit'}/>
-  </div>
+      <ScrollComponent
+        apiUrl={"https://jsonplaceholder.typicode.com/photos"}
+        pageNumber={"_page"}
+        pageLimit={"_limit"}
+      />
+    </div>
   );
 }
 // another url supporting pagination : https://jsonplaceholder.typicode.com/comments
